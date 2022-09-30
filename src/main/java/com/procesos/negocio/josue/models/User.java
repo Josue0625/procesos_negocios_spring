55b497c2aca78b8350e33f55c18dfa25a7402e44
Josue0625 <jcampos@ufpso.edu.co>
2022-09-29 21:09:36 -0500

    validaciones persistencia y codigos de estado http

--- src/main/java/com/procesos/negocio/josue/models/User.java
+++ src/main/java/com/procesos/negocio/josue/models/User.java
@@ -13,11 +13,16 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length = 100, nullable = false)
     private String name;
+    @Column(length = 300, nullable = false)
     private String lastname;
+    @Column(length = 20, nullable = false)
     private String document;
+    @Column(length = 100)
     private String direction;
     private Date Date_Of_Birth;
+    @Column(length = 20)
     private String phone;
 
 }
